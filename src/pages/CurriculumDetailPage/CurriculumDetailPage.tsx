f3f13c460859767f020bbf2569ed0c08de05f259
scottWhiteman <scooterjmail@gmail.com>
2021-07-29 13:58:54 -0700

    CurriculumDetail Page

---
+++ src/pages/CurriculumDetailPage/CurriculumDetailPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CurriculumDetailPage = () => {
+  return (
+    <div>
+      <aside className='curriculum-tabs'>
+
+      </aside>
+      <article className='tab-details'>
+        
+      </article>
+    </div>
+  )
+}
+
+export default CurriculumDetailPage;
