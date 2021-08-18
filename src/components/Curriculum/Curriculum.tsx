dfcbf63023a27774b432110db39183ad767ae6a2
scottWhiteman <scooterjmail@gmail.com>
2021-08-18 13:56:32 -0700

    comment cleanup Curriculum

--- src/components/Curriculum/Curriculum.tsx
+++ src/components/Curriculum/Curriculum.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-// import { Dispatch } from 'redux';
-// import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 type Props = {
@@ -8,18 +6,6 @@ type Props = {
 }
 
 const Curriculum: React.FC<Props> = ({ curriculum }) => {
-  // const dispatch:Dispatch<any> = useDispatch();
-  // const history = useHistory();
-  // const { id } = useParams();
-
-  // const handleSelect = useCallback(
-    //(selectedCurriculum:Curriculum) => dispatch(selectCurriculum(selectedCurriculum)),
-    // (selectedCurriculum:Curriculum) => {
-    //   dispatch(selectCurriculum(selectedCurriculum));
-    //   history.push(`/curriculums/${curriculum.id}`);
-    // },
-    // [dispatch, history]
-  // );
 
   return (
     <Link className='curriculum' to={`/curriculums/${curriculum.id}`}>
